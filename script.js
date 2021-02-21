2439c7e3ef5691a6cef316473892b966cb8b3be9
Daniel Kuc <danielpiotrkuc@gmail.com>
2021-02-21 18:31:23 +0000

    scripts added: state, display data from state, created list of items and injected into DOM, added app init function.

---
+++ script.js
@@ -0,0 +1,76 @@
+// const balance = document.querySelector('#balance');
+// const moneyPlus = document.querySelector('#money-plus');
+// const moneyMinus = document.querySelector('#money-minus');
+// const list = document.querySelector('#list');
+const form = document.querySelector('#form');
+// const text = document.querySelector('#text');
+// const amount = document.querySelector('#amount');
+
+const dummyTransactions = [
+  {id:1, text: 'Flower', amount:-20},
+  {id:2, text: 'Salary', amount: 300},
+  {id:3, text: 'Book', amount:-10},
+  {id:4, text: 'Camera', amount: 150}
+];
+
+let transactions = dummyTransactions;
+// state
+
+const addTransaction = e => {
+  e.preventDefault();
+
+  if($('#text').val().trim() === '' || $('#amount').val().trim()) {
+    alert('Please add valid input')
+  }
+
+};
+
+// add transactions to dom
+const addTransactionDOM = (transaction) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class="delete-btn">x</button>
+  `;
+
+  $('#list').append(item);
+};
+
+// update the balance, income and expense
+
+const updateValues = () => {
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => acc += item,0).toFixed(2);
+  $('#balance').html(`£${total}`);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => acc += item, 0)
+    .toFixed(2);
+  $('#money-plus').html(`£+${income}`);
+
+  const expense = amounts
+    .filter(item => item < 0)
+    .reduce((acc, item) => acc += item, 0)
+    .toFixed(2);
+  $('#money-minus').html(`£${expense}`);
+};
+
+
+
+// App init
+const init = () => {
+  $('#list').html('');
+
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+};
+
+init();
+
+// form.addEventListener('submit', addTransaction());
+
+$('#form').submit(addTransaction);
